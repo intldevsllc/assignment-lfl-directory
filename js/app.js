ff5b1681c188a07c043d032520a9c44228c87035
Pablo Arcia <intldevsllc@gmail.com>
2022-01-26 15:35:37 +0500

    add page content

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
+const Content = function () {
+  const content = create('div', 'content')
+  const { search } = window.location
+
+  if (search.includes('page=add')) {
+  } else if (search.includes('page=verify')) {
+  } else if (search.includes('page=update')) {
+  } else if (search.includes('page=delete')) {
+  } else {
+  }
+
+  return content
+}
+
 const App = function () {
   const app = create('div', 'app')
   const sidebar = Sidebar()
+  const content = Content()
+
   app.append(sidebar)
+  app.append(content)
 
   return app
 }
